dc74b632e053c423a7f755039a0dc6b4d427d242
philokaram <146956626+philokaram@users.noreply.github.com>
2024-10-24 09:01:04 +0300

    add functions to read and write form/to console

--- Lossless_algorithems/LZ77/Dictionary_based/LZ77.java
+++ Lossless_algorithems/LZ77/Dictionary_based/LZ77.java
@@ -3,27 +3,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class LZ77 {
-
     static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("    _________________________________________________  ");
-        System.out.println("   |                                                 | ");
-        System.out.println("   |      welcome to LZ77 compression algorithm      | ");
-        System.out.println("   |_________________________________________________| \n");
-        System.out.println("Enter 1 to Comperes or Decompress in the consol.\nEnter 2 to Comperes or Decompress a txt file.\n");
-        System.out.print("Enter your choice: ");
-        // int choice = input.nextInt();
-        //  if (choice == 1) {
-        Comperes();
-//} else if (choice == 2) {
-
-//} else {
-//}
-    }
-
     static class Tag {
-
         public int position;
         public int length;
         public char nextSymbol;
@@ -49,24 +31,78 @@ class LZ77 {
                 maxLength = arr.get(i).length;
                 maxILength = i;
             }
-
         }
         return arr.get(maxILength);
     }
 
-    public static void Comperes() {
+    static public String read(char from, char operation) {
+        String text = "";
+        if (from == 'c') {
+            if (operation == 'c') {
+                System.out.println("Enter your text to compress it (Enter \\0 to end reading): ");
+            } else {
+                System.out.println("Enter your tags to decompress it (Enter \\0 to end reading): ");
+            }
+            while(true){
+                String line = input.nextLine();
+                if(line.charAt(0)== '\\' && line.charAt(1)== '0'  ){
+                    break;
+                }
+                text += line;
+            }
+            return text;
+        }
+        return text;
+    }
+    static public void write(String text,char from, char operation) {
+        if (from == 'c') {
+            if (operation == 'c') {
+                System.out.println("the compressed text: ");
+            } else {
+                System.out.println("the decompressed Tags: ");
+            }
+           System.out.println(text);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("    _________________________________________________  ");
+        System.out.println("   |                                                 | ");
+        System.out.println("   |      welcome to LZ77 compression algorithm      | ");
+        System.out.println("   |_________________________________________________| \n");
+        System.out.println("Enter 1 to Compress text from the consol.");
+        System.out.println("Enter 2 to Decompress tags from the consol.");
+        System.out.println("Enter 3 to Compress a txt file.");
+        System.out.println("Enter 4 to Decompress a txt file.");
+        System.out.print("Enter your choice: ");
+        int choice = input.nextInt();
+        input.nextLine();
+        String in_text;
+        String out_text;
+        if (choice == 1) {
+            in_text = read('c', 'c');
+            out_text =compress(in_text,10,10);
+            write(out_text, 'c', 'c');
+        } else if (choice == 2) {
+            in_text = read('c', 'd');
+            out_text = decompress(in_text);
+            write(out_text, 'c', 'd');
+        } else if (choice == 3) {
+            in_text = read('f', 'c');
+        } else if (choice == 4) {
+            in_text = read('f', 'd');
+        } else {
+        }
+    }
+
+    public static String compress(String text , int search_window_size ,int look_ahead_window_size  ) {
         ArrayList<Tag> tags = new ArrayList<>();
-        int search_window_size = 10;
-        int look_ahead_window_size = 10;
-        System.out.print("Enter your text to compress it: ");
-        // input.nextLine();
-        String text = input.nextLine();
         tags.add(new Tag(0, 0, text.charAt(0)));
-        for (int i = 1; i < text.length(); i++) { //text
+        for (int i = 1; i < text.length(); i++) { // text
             ArrayList<Tag> tags_for_symbol = new ArrayList<>();
             for (int j = i - 1; j >= 0 && i - j <= search_window_size; j--) {
                 int length = 0;
-                for (int k = 0; (k <= look_ahead_window_size) && ((i + k) < text.length()-1) && ((j + k) < i); k++) {
+                for (int k = 0; (k <= look_ahead_window_size) && ((i + k) < text.length() - 1) && ((j + k) < i); k++) {
                     if (text.charAt(i + k) == text.charAt(j + k)) {
                         length++;
                     } else {
@@ -77,11 +113,10 @@ class LZ77 {
                     tags_for_symbol.add(new Tag(0, 0, text.charAt(i)));
                 } else {
                     char nextSymbol;
-                    if ((i == text.length() - 1)&&(i+length >= text.length()-1) ) {
+                    if ((i == text.length() - 1) && (i + length >= text.length() - 1)) {
                         nextSymbol = '_';
-                    }
-                    else {
-                        nextSymbol = text.charAt(i+length);
+                    } else {
+                        nextSymbol = text.charAt(i + length);
                     }
                     tags_for_symbol.add(new Tag(i - j, length, nextSymbol));
                 }
@@ -90,9 +125,26 @@ class LZ77 {
             tags.add(x);
             i += x.length;
         }
+        String outputString="";
         for (int i = 0; i < tags.size(); i++) {
-            System.out.print("<" + tags.get(i).position + "," + tags.get(i).length + "," + tags.get(i).nextSymbol + ">");
+            outputString += ("<" + tags.get(i).position + "," + tags.get(i).length + "," + tags.get(i).nextSymbol + ">");
         }
+        return outputString;
     }
-    
+
+    public static String decompress(String input_tags) {
+        String decompressed_text = "";
+        for (int i = 0; i < input_tags.length(); i++) {
+            if (input_tags.charAt(i) == '<') {
+                int position = (input_tags.charAt((i + 1)) - '0');
+                int length = (input_tags.charAt((i + 3)) - '0');
+                for (int j = decompressed_text.length() - position; length > 0; length--, j++) {
+                    decompressed_text += decompressed_text.charAt(j);
+                }
+                decompressed_text += input_tags.charAt(i + 5);
+                i += 6;
+            }
+        }
+        return decompressed_text;
+    }
 }
